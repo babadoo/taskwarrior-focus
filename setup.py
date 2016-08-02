0ec77878b6aa2ad991246fc2c94328c3fa009e25
babadoo <babadoo@doonx.org>
2016-08-02 20:52:53 +0200

    added focus report command example to readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='taskwarrior-focus',
-    version='0.1.4',
+    version='0.1.5',
     description='Taskwarrior wrapper for manual ordering of a focus list',
     url='http://github.com/babadoo/taskwarrior-focus',
     author='babadoo',
