6c43aa275e168a61800e4bfdf8c4fb21d28430bf
babadoo <babadoo@doonx.org>
2016-08-02 20:03:45 +0200

    bumped hotfix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='taskwarrior-focus',
-    version='0.1',
+    version='0.1.1',
     description='Taskwarrior wrapper for manual ordering of a focus list',
     url='http://github.com/babadoo/tw',
     author='babadoo',
