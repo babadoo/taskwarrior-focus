b45e13013ebe2213dc796b4ffcb0122ff7243eb6
Christof Hagedorn <christof.hagedorn@kinea.de>
2017-11-22 22:46:39 +0100

    merge hours of tasklist with default range of 8 to 23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='taskwarrior-focus',
-    version='0.1.7',
+    version='0.1.8',
     description='Taskwarrior wrapper for manual ordering of a focus list',
     url='http://github.com/babadoo/taskwarrior-focus',
     author='babadoo',
