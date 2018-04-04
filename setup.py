1faabcbb57a0aef99a204b218a6947c22129d523
babadoo <babadoo@doonx.org>
2018-04-04 08:17:52 +0200

    note command

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='taskwarrior-focus',
-    version='0.1.6',
+    version='0.1.7',
     description='Taskwarrior wrapper for manual ordering of a focus list',
     url='http://github.com/babadoo/taskwarrior-focus',
     author='babadoo',
